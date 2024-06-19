e2581788cce89179d919c5819ae6f1acd1711a15
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 18:17:04 +0200

    React 'Only React_01' ! Babel jsx added div container  !

--- main.js
+++ main.js
@@ -5,28 +5,11 @@ const container = document.querySelector(".container");
 const root = ReactDOM.createRoot(container);
 
 const titleReact = <h1 className="title__React-App">React App</h1>; 
+
 const buttonReact = <button className="btn" type="submit">React</button>
 
-// const buttonReact = React.createElement(
-//   "button",
-//   {
-//     className: "btn",
-//     type: "submit",
-//     onClick: () => {
-//       console.log("clicked");
-//     },
-//   },
-//   "React"
-// );
+const boxReact = <div className="box__React">{titleReact}{buttonReact}</div>
 
-// const boxReact = React.createElement(
-//   "div",
-//   {
-//     className: "box__React",
-//   },
-//   [titleReact, buttonReact]
-// );
 
-// root.render(boxReact);
+root.render(boxReact);
 
-console.log(titleReact, buttonReact);
