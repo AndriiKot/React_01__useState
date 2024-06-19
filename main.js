13fde9e16f1d064d00fcc6ac316a894ed9576549
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 09:10:07 +0200

    React 'Only React_01' ! create DOMVanillaJS button testing 1 !

--- main.js
+++ main.js
@@ -3,4 +3,7 @@
 const container1 = document.querySelector(".container-1");
 const container2 = document.querySelector(".container-2");
 
-console.log(container1, container2);
+
+const buttonVanillaJs = document.createElement("button");
+
+console.log(buttonVanillaJs);
