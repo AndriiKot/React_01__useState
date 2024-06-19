4572d40bf39207ddd2d605439554435c61b6d4c7
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 09:08:22 +0200

    React 'Only React_01' ! create .container-1 and .container-2 testing 1 !

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-﻿console.log("HI!!!")
+﻿"use strict";
+
+const container1 = document.querySelector(".container-1");
+const container2 = document.querySelector(".container-2");
+
+console.log(container1, container2);
