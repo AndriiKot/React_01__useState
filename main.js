68652e03376c347e2031828e03817b7397e2440d
AndriiKot <a_kocyuba@ukr.net>
2024-06-19 09:42:11 +0200

    React 'Only React_01' ! create ReactElments testing 1 !

--- main.js
+++ main.js
@@ -3,10 +3,13 @@
 const container1 = document.querySelector(".container-1");
 const container2 = document.querySelector(".container-2");
 
-
+// VanillaJS
 const buttonVanillaJs = document.createElement("button");
 buttonVanillaJs.textContent = "Vanilla JS"; 
 // or buttonVanillaJs.innerText = "Vanilla JS";
 
 container1.appendChild(buttonVanillaJs);
 
+// React
+const buttonReact = React.createElement("button");
+console.log(buttonReact);
