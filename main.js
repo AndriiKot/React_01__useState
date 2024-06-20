2fd078fa151fb91f6f46d90af266b870378913a7
AndriiKot <a_kocyuba@ukr.net>
2024-06-20 19:19:33 +0200

    React 'Only React_01' ! Restart 'State__React_Component_Dynamic_v1.0.1'  !

--- main.js
+++ main.js
@@ -6,17 +6,25 @@ const root = ReactDOM.createRoot(container);
 
 <h1 className="title__React-App">React App</h1>;
 
-const boxReact = (
-  <div className="box__React">
-    <h1 className="title__React-App">React App</h1>
-    <button
-      className="btn"
-      type="submit"
-      onClick={() => console.log("Clicked!")}
-    >
-      React
-    </button>
-  </div>
-);
+function App(props) {
+  const [counter, setCounter] = React.useState(0);
+  const [hiddenTitle, setHiddenTitle] = React.useState(true);
 
-root.render(boxReact);
+  return (
+    <div>
+      {hiddenTitle && <h1>{props.title}</h1>}
+      <button
+        className="btn"
+        type="submit"
+        onClick={() => setCounter(counter + 1)}
+      >
+        {props.labeledButton} {counter}
+      </button>
+      <button onClick={() => setHiddenTitle(!hiddenTitle)}>
+        {!hiddenTitle ? "Show" : "Hide"} Title
+      </button>
+    </div>
+  );
+}
+
+root.render(<App title="React App" labeledButton="Counter: " />);
