77449a689a309ecb86eb93d8ad45a733538a533c
AndriiKot <a_kocyuba@ukr.net>
2024-06-20 19:09:03 +0200

    React 'Only React_01' ! Bag FIXED README.md 'Babel__jsx_ReactElement__v1.0.0'  !

--- main.js
+++ main.js
@@ -4,32 +4,19 @@ const container = document.querySelector(".container");
 
 const root = ReactDOM.createRoot(container);
 
-const titleReact = React.createElement(
-  "h1",
-  {
-    className: "title__React-App",
-  },
-  "React App"
+const titleReact = <h1 className="title__React-App">React App</h1>;
+
+const buttonReact = (
+  <button className="btn" type="submit" onClick={() => console.log("Clicked!")}>
+    React
+  </button>
 );
 
-const buttonReact = React.createElement(
-  "button",
-  {
-    className: "btn",
-    type: "submit",
-    onClick: () => {
-      console.log("clicked");
-    },
-  },
-  "React"
-);
-
-const boxReact = React.createElement(
-  "div",
-  {
-    className: "box__React",
-  },
-  [titleReact, buttonReact]
+const boxReact = (
+  <div className="box__React">
+    {titleReact}
+    {buttonReact}
+  </div>
 );
 
 root.render(boxReact);
