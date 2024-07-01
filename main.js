15e8e2637e9650cd0b420c4ffef2d83afb02d5da
AndriiKot <a_kocyuba@ukr.net>
2024-07-01 21:04:07 +0200

    React 'Only React_01' !  added NEW Function visibility: hidden !

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ function App(props) {
 
   return (
     <div className="App">
-      {hiddenTitle && <h1>{props.title}</h1>}
+      {hiddenTitle ? (
+        <h1>{props.title}</h1>
+      ) : (
+        <h1 className="hidden">{props.title}</h1>
+      )}
       <button
         className="btn"
         type="submit"
@@ -21,7 +25,7 @@ function App(props) {
         {props.labeledButton} {counter}
       </button>
       <button onClick={() => setHiddenTitle(!hiddenTitle)}>
-        {!hiddenTitle ? "Show" : "Hide"} Title
+        {!hiddenTitle ? "Show" : "Hide  "} App
       </button>
     </div>
   );
